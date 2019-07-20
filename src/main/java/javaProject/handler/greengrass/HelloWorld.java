1707499463c97ea947ab5e9cd6ef88ba6c4e0d5e
yukikodama <Courage123456!>
2019-07-21 01:21:05 +0900

    first commit.

---
+++ src/main/java/javaProject/handler/greengrass/HelloWorld.java
@@ -0,0 +1,36 @@
+package javaProject.handler.greengrass;
+
+import java.nio.ByteBuffer;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.greengrass.javasdk.IotDataClient;
+import com.amazonaws.greengrass.javasdk.model.*;
+
+
+public class HelloWorld {
+    static {
+        Timer timer = new Timer();
+        // Repeat publishing a message every 5 seconds
+        timer.scheduleAtFixedRate(new PublishHelloWorld(), 0, 5000);
+    }
+
+    public String handleRequest(Object input, Context context) {
+        return "Hello from java";
+    }
+}
+
+class PublishHelloWorld extends TimerTask {
+    private IotDataClient iotDataClient = new IotDataClient();
+    private String publishMessage = String.format("Hello world! Sent from Greengrass Core running on platform: %s-%s using Java", System.getProperty("os.name"), System.getProperty("os.version"));
+    private PublishRequest publishRequest = new PublishRequest().withTopic("hello/world").withPayload(ByteBuffer.wrap(String.format("{\"message\":\"%s\"}", publishMessage).getBytes()));
+
+    public void run() {
+        try {
+            iotDataClient.publish(publishRequest);
+        } catch (Exception ex) {
+            System.err.println(ex);
+        }
+    }
+}
