926e7e341e45c3584e9f80834412f2d3031634bd
yuki-kodama <kodama@ap-com.co.jp>
2019-07-23 12:46:20 +0900

    first commit.

--- src/main/java/javaProject/handler/greengrass/HelloWorld.java
+++ src/main/java/javaProject/handler/greengrass/HelloWorld.java
@@ -1,8 +1,12 @@
 package javaProject.handler.greengrass;
 
 import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.greengrass.javasdk.IotDataClient;
@@ -22,8 +26,19 @@ public class HelloWorld {
 
 class PublishHelloWorld extends TimerTask {
     private IotDataClient iotDataClient = new IotDataClient();
-    private String publishMessage = String.format("Hello world! Sent from Greengrass Core running on platform: %s-%s using Java", System.getProperty("os.name"), System.getProperty("os.version"));
-    private PublishRequest publishRequest = new PublishRequest().withTopic("hello/world").withPayload(ByteBuffer.wrap(String.format("{\"message\":\"%s\"}", publishMessage).getBytes()));
+    private final String publishMessage;
+    private final PublishRequest publishRequest;
+    public PublishHelloWorld() {
+        String serial = null;
+        try {
+            Matcher m = Pattern.compile("Serial\\s+:\\s+(\\w{16})").matcher(new String(Files.readAllBytes(Paths.get("/proc/cpuinfo"))));
+            serial = m.find() ? m.group(1): "none";
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+        publishMessage = String.format("Hello world! Sent from Greengrass Core running on platform: %s-%s, Serial: %s", System.getProperty("os.name"), System.getProperty("os.version"), serial);
+        publishRequest = new PublishRequest().withTopic("hello/world").withPayload(ByteBuffer.wrap(String.format("{\"message\":\"%s\"}", publishMessage).getBytes()));
+    }
 
     public void run() {
         try {
