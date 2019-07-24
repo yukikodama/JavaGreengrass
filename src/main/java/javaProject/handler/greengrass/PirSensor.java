0044c93caa5ca46c987be329ba4493a804e72ca9
kodama <kodama@ap-com.co.jp>
2019-07-24 18:34:14 +0900

    first commit.

---
+++ src/main/java/javaProject/handler/greengrass/PirSensor.java
@@ -0,0 +1,57 @@
+package javaProject.handler.greengrass;
+
+import com.amazonaws.greengrass.javasdk.IotDataClient;
+import com.amazonaws.greengrass.javasdk.model.PublishRequest;
+import com.amazonaws.services.lambda.runtime.Context;
+import org.iot.raspberry.grovepi.GroveDigitalOut;
+import org.iot.raspberry.grovepi.pi4j.GrovePi4J;
+import org.json.JSONObject;
+
+import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PirSensor extends TimerTask {
+    private static final String TOPIC = "topic/pirsensor";
+    private static final String CPUINFO = "/proc/cpuinfo";
+
+    private IotDataClient iotDataClient = new IotDataClient();
+    private String serial;
+    private GroveDigitalOut digitalOut4;
+    private boolean status;
+
+    static {
+        Timer timer = new Timer();
+        try {
+            timer.scheduleAtFixedRate(new PirSensor(), 0, 10000);
+        } catch (Exception ex) {
+            System.err.println(ex);
+        }
+    }
+
+    private PirSensor() throws Exception {
+        Matcher m = Pattern.compile("Serial\\s+:\\s+(\\w{16})").matcher(new String(Files.readAllBytes(Paths.get(CPUINFO))));
+        serial = m.find() ? m.group(1) : "none";
+        digitalOut4 = new GrovePi4J().getDigitalOut(4);
+    }
+
+    @Override
+    public void run() {
+        try {
+            digitalOut4.set(status);
+            status = !status;
+            String publishMessage = new JSONObject().put("SensorId", serial).put("pir", "value").toString();
+            iotDataClient.publish(new PublishRequest().withTopic(TOPIC).withPayload(ByteBuffer.wrap(publishMessage.getBytes())));
+        } catch (Exception ex) {
+            System.err.println(ex);
+        }
+    }
+
+    public String handleRequest(Object input, Context context) {
+        return "ok";
+    }
+}
