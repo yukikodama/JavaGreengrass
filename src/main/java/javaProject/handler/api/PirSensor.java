6840e3609f7362fb42d583812b7b2b7ce2f25205
yukikodama <Courage123456!>
2019-07-28 00:51:51 +0900

    first commit.

--- src/main/java/javaProject/handler/api/PirSensor.java
+++ src/main/java/javaProject/handler/api/PirSensor.java
@@ -26,14 +26,15 @@ public class PirSensor implements RequestHandler<APIGatewayProxyRequestEvent, AP
         logger.log("context: " + context);
 
         ScanResult scanResult = amazonDynamoDB.scan(new ScanRequest().withTableName("Sensor"));
-        Stream<String> sensorIds = scanResult.getItems().stream().map(item -> item.get("SensorId").getS());
-        sensorIds.forEach(sensorId -> {
-            Map<String, AttributeValue> values = new HashMap<>();
-            values.put(":s", new AttributeValue().withS(sensorId));
-            QueryRequest queryRequest = new QueryRequest().withTableName("PirSensor").withKeyConditionExpression("SensorId = :s").withExpressionAttributeValues(values).withLimit(10).withScanIndexForward(false);
-            QueryResult queryResult = amazonDynamoDB.query(queryRequest);
-            logger.log("queryResult: " + queryResult.getItems());
-        });
+        logger.log(scanResult.toString());
+//        Stream<String> sensorIds = scanResult.getItems().stream().map(item -> item.get("SensorId").getS());
+//        sensorIds.forEach(sensorId -> {
+//            Map<String, AttributeValue> values = new HashMap<>();
+//            values.put(":s", new AttributeValue().withS(sensorId));
+//            QueryRequest queryRequest = new QueryRequest().withTableName("PirSensor").withKeyConditionExpression("SensorId = :s").withExpressionAttributeValues(values).withLimit(10).withScanIndexForward(false);
+//            QueryResult queryResult = amazonDynamoDB.query(queryRequest);
+//            logger.log("queryResult: " + queryResult.getItems());
+//        });
 
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
